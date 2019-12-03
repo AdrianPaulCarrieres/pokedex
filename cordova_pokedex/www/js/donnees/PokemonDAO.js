3e58e0646aa3c535abf3d0fe749203e9aca81764
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2019-12-03 11:39:01 -0500

    ajout fonction lister

--- cordova_pokedex/www/js/donnees/PokemonDAO.js
+++ cordova_pokedex/www/js/donnees/PokemonDAO.js
@@ -7,4 +7,17 @@ var PokemonDAO = function() {
         }
     }
 
+    this.lister = function() {
+        if (localStorage['pokemon']) {
+            listePokemon = JSON.parse(localStorage['pokemon']);
+        }
+
+        for (position in listePokemon) {
+            var pokemon = new PokemonDAO(listePokemon[position].nom, listePokemon[position].type, listePokemon[position].pv, listePokemon[position].pc, listePokemon[position].image, listePokemon[position].son)
+            listePokemon[poisition] = pokemon
+        }
+
+        return listePokemon;
+    }
+
 }();
