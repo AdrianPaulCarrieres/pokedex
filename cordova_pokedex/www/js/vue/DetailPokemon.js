c86758281d3145ef249ef0590211f572bc47b647
Louis Pascuzzi <louis.pascuzzi@edu.univ-fcomte.fr>
2019-12-05 17:37:04 -0500

    ajout de deux graphiques sur la vue detail pokemon

--- cordova_pokedex/www/js/vue/DetailPokemon.js
+++ cordova_pokedex/www/js/vue/DetailPokemon.js
@@ -9,6 +9,56 @@ var DetailPokemon = (function() {
 
             elementBody = document.getElementsByTagName("body")[0];
             elementBody.innerHTML = fragmentBarreNavigationHaut + pageDetailPokemon;
+            var ctx = document.getElementById('graphiquePv').getContext('2d');
+            var chart = new Chart(ctx, {
+
+                type: 'bar',
+                data: {
+                    labels: ["Qulbutoké", "Psychokwak"],
+                    datasets: [
+                        {
+                            label: "PV",
+                            backgroundColor: ["#3e95cd", "#8e5ea2"],
+                            data: [4320,5267]
+                        }
+                    ]
+                },
+                options: {
+                    legend: { display: false },
+                    title: {
+                        display: true,
+                        text: 'PVs des pokémons du type :'
+                    }
+                }
+
+            });
+            var ctx = document.getElementById('graphiquePc').getContext('2d');
+            var chart = new Chart(ctx, {
+
+                type: 'bar',
+                data: {
+                    labels: ["Qulbutoké", "Psychokwak"],
+                    datasets: [
+                        {
+                            label: "PC",
+                            backgroundColor: ["#3e95cd", "#8e5ea2"],
+                            data: [4320,5267]
+                        }
+                    ]
+                },
+                options: {
+                    legend: { display: false },
+                    title: {
+                        display: true,
+                        text: 'PCs des pokémons du type :'
+                    }
+                }
+
+            });
+
+
+
+            $('.carousel').carousel('pause');
         }
     }
 })();
