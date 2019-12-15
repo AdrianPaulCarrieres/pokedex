97f73c762900097854b09846b77af18d2d0b477c
Louis Pascuzzi <louis.pascuzzi@edu.univ-fcomte.fr>
2019-12-15 16:48:03 -0500

    ajout de la fonction enregistrer dans AjouterPokemonVue

--- cordova_pokedex/www/js/vue/AjouterPokemonVue.js
+++ cordova_pokedex/www/js/vue/AjouterPokemonVue.js
@@ -6,9 +6,25 @@ var AjouterPokemonVue = (function() {
     
     return function(actionAjouterPokemon) {
         this.afficher = function() {
-
             elementBody = document.getElementsByTagName("body")[0];
-            elementBody.innerHTML = fragmentBarreNavigationHaut + pageAJouterPokemon + fragmentBarreNavigationBas;        
+            elementBody.innerHTML = fragmentBarreNavigationHaut + pageAJouterPokemon + fragmentBarreNavigationBas;
+
+            var formulaireAjouter = document.getElementById("formulaire-ajouter");
+            formulaireAjouter.addEventListener("submit", enregistrer);
         }
+
+        var enregistrer = function(evenement){
+            evenement.preventDefault();
+
+            var nom = document.getElementById("ajouter-nom");
+            var type = document.getElementById("ajouter-type");
+            var image = document.getElementById("ajouter-image");
+            var pv = document.getElementById("ajouter-pv");
+            var pc = document.getElementById("ajouter-pc");
+
+            var pokemon = new Pokemon(nom, type, image, pv, pc);
+            actionAjouterPokemon(pokemon);
+        }
+
     }
 })();
