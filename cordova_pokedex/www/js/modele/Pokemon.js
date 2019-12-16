ed26da4eae1c50164eb811713018ae8af15bc98f
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2019-12-16 12:09:47 -0500

    ajout modele pokemon

---
+++ cordova_pokedex/www/js/modele/Pokemon.js
@@ -0,0 +1,9 @@
+var Cadeau = function(nom, type, cheminImage, pv, pc, chemimSon, id) {
+    this.id = id;
+    this.nom = nom;
+    this.type = type;
+    this.cheminImage = cheminImage;
+    this.pv = pv;
+    this.pc = pc;
+    this.chemimSon = chemimSon;
+};
