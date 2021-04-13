ff8cf3abd074151e9ff5bb47e58f7c55ddd4f539
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2021-04-13 10:16:09 +0200

    ??

--- cordova_pokedex/platforms/android/app/src/main/assets/www/js/modele/Pokemon.js
+++
@@ -1,9 +0,0 @@
-var Pokemon = function(nom, type, cheminImage, pv, pc, id) {
-    this.id = id;
-    this.nom = nom;
-    this.type = type;
-    this.cheminImage = cheminImage;
-    this.pv = pv;
-    this.pc = pc;
-    //this.chemimSon = chemimSon;
-};
